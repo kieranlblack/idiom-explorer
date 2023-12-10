126ad047889813c14b862ec544857df54d8d2055
Kieran Black <kieranlblack@gmail.com>
2023-12-10 03:39:01 -0500

    add instructions to search graph

--- src/IdiomSearchGraphPage.js
+++ src/IdiomSearchGraphPage.js
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from "react";
 
 import MultiSelectTextInput from "./MultiSelectTextInput";
 import ExpandableGraph from "./ExpandableGraph";
-import { Button, Stack } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import Loading from "./Loading";
 import { toast } from "react-toastify";
 import { useSearchParams } from "react-router-dom";
@@ -72,8 +72,24 @@ const IdiomSearchGraphPage = () => {
     setSelectedIdioms([randomIdiom]);
   };
 
+  const [infoHidden, setInfoHidden] = useState(false);
+
+  const hideInfo = () => {
+    setInfoHidden(true);
+  };
+
   return loaded ? (
     <Stack spacing={1} style={{ flex: 1, display: "flex" }}>
+      {!infoHidden && (
+        <div style={{ border: "1px dashed" }} onClick={hideInfo}>
+          <Typography style={{ padding: 3 }}>
+            点击顶以探索图。<span style={{ color: "fuchsia" }}>粉色</span>
+            的顶点属于接龙，<span style={{ color: "green" }}>绿色</span>表示还隐藏了边，
+            <span style={{ color: "red" }}>红色</span>表示没有边，<span style={{ color: "orange" }}>橙色</span>
+            表示被展开的。顶点的大小取决于成语的出现频率。点击隐藏我。
+          </Typography>
+        </div>
+      )}
       <Stack spacing={2} direction={"row"} style={{ display: "flex", alignItems: "center" }}>
         <div style={{ flex: 0 }}>
           <Button variant="outlined" onClick={setRandomIdiom}>
