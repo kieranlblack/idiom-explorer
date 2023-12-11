0e4c5d4fa5c84f7efb8f7484e6c24f875cb225ce
Kieran Black <kieranlblack@gmail.com>
2023-12-11 12:13:56 -0500

    weight dijkstras

--- src/idioms.js
+++ src/idioms.js
@@ -7,10 +7,20 @@ export const getIdiomInfo = (idiom) => {
   return idiomLookup[idiom];
 };
 
+export const weightFunction = (frequency) => {
+  if (frequency === 0) {
+    return 1 / 1e-15;
+  }
+  if (frequency < 1e-4) {
+    return 1 / frequency;
+  }
+  return 1;
+};
+
 export const findShortestPath = (startIdiom, endIdiom, graphData) => {
   const graph = Object.fromEntries(graphData.nodes.map((node) => [node, {}]));
   for (const [src, dst] of graphData.edges) {
-    graph[src][dst] = 1;
+    graph[src][dst] = weightFunction(getIdiomInfo(dst).frequency);
   }
 
   try {
